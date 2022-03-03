14c86b7715f0f627758eeda594ac0929565f70d1
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-03-03 22:59:35 +0300

    normal start

--- MyTrain/MyTrain/apps/horoscope/urls.py
+++ MyTrain/MyTrain/apps/horoscope/urls.py
@@ -5,5 +5,14 @@ from . import views
 # Ссылки по которым можно перейти на станицу
 app_name = 'horoscope'
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"), # Главная
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+    path('', views.index, name="index"), 
+
 ]
--- MyTrain/MyTrain/apps/horoscope/views.py
+++ MyTrain/MyTrain/apps/horoscope/views.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 
 def index(request):
 
+    signs_of_zodiac = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Aquarius', 'Pisces']
+
     return render(
         request,
         template_name='horoscope/index.html',
-        context={"title": "horoscope"}
+        context={"title": "horoscope", "signs_of_zodiac": signs_of_zodiac}
     )
     # return HttpResponse("Hello, World!")
