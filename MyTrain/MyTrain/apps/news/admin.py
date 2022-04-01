a43e7a9d6893587beeb6de05ea70e920cb0d1df3
KodokuOdius <alanfoxincoolctranica@gmail.com>
2022-04-01 09:44:21 +0300

    sleep

--- MyTrain/MyTrain/apps/news/admin.py
+++ MyTrain/MyTrain/apps/news/admin.py
@@ -1,11 +1,13 @@
+
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 # Класс в админки
 class NewsAdmin(admin.ModelAdmin):
     # Атрибуты для отображения админки
-    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published', 'get_photo')
 
     # Атрибуты с сылками для редактирования
     list_display_links = ('id', 'title')
@@ -19,6 +21,19 @@ class NewsAdmin(admin.ModelAdmin):
     # Фильтр
     list_filter = ('is_published', 'category')
 
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published',
+    'views', 'created_at', 'updated_at')
+
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+        else:
+            return "-"
+
+    get_photo.short_desctiption = "Миниатюра"
 
 
 class CategoryAdmin(admin.ModelAdmin):
